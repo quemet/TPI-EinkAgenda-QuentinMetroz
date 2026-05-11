a5cc421c0df64d8bee80235fcd6ef6adbca3e540
Quentin Métroz <pg03lul@eduvaud.ch>
2026-05-11 11:19:56 +0200

    feat: add the association between the models and test if it's create table in db : ok

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,56 @@
 import { Sequelize } from 'sequelize';
 import { env } from './env';
+import Agenda from '../models/agenda.model';
+import Appertain from '../models/appertain.model';
+import Belong from '../models/belong.model';
+import Event from '../models/event.model';
+import Family from '../models/family.model';
+import User from '../models/user.model';
 
 const sequelize = new Sequelize(env.DB.NAME, env.DB.USER, env.DB.PASSWORD, {
   host: env.DB.HOST,
   port: Number(env.DB.PORT) || 3306,
   dialect: 'mysql',
   logging: false,
+});
+
+Agenda.initModel(sequelize);
+Appertain.initModel(sequelize);
+Belong.initModel(sequelize);
+Event.initModel(sequelize);
+Family.initModel(sequelize);
+User.initModel(sequelize);
+
+Family.hasMany(Agenda, { foreignKey: 'familyId' });
+Agenda.belongsTo(Family, { foreignKey: 'familyId' });
+
+Agenda.hasMany(Event, { foreignKey: 'agendaId' });
+Event.belongsTo(Agenda, { foreignKey: 'agendaId' });
+
+Family.belongsToMany(User, {
+  through: Belong,
+  foreignKey: 'family_id',
+  otherKey: 'user_id',
+});
+
+User.belongsToMany(Family, {
+  through: Belong,
+  foreignKey: 'user_id',
+  otherKey: 'family_id',
+});
+
+Family.belongsToMany(User, {
+  through: Appertain,
+  foreignKey: 'family_id',
+  otherKey: 'family_adminId',
+  as: 'admins',
+});
+
+User.belongsToMany(Family, {
+  through: Appertain,
+  foreignKey: 'family_adminId',
+  otherKey: 'family_id',
+  as: 'administeredFamilies',
 });
 
 const initDatabase = async () => {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
 import { initDatabase } from './config/db';
-import { env } from './config/env';
 
-if (env.NODE_ENV !== 'test') {
-  initDatabase()
-    .then(() => {
-      console.info('Database initialized successfully.');
-    })
-    .catch((err) => {
-      console.error('Failed to initialize database:', err);
-      process.exit(1);
-    });
-}
+initDatabase()
+  .then(() => {
+    console.info('Database initialized successfully.');
+  })
+  .catch((error) => {
+    console.error('Failed to initialize database:', error);
+    process.exit(1);
+  });
