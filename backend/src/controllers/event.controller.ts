b67dbe980e2245733f364845d9dd9cc0d45b4169
Quentin Métroz <pg03lul@eduvaud.ch>
2026-05-13 08:43:34 +0200

    feat: add the controller for the event

---
+++ backend/src/controllers/event.controller.ts
@@ -0,0 +1,59 @@
+import * as eventService from '../services/event.service';
+import ExtendedRequest from '../types/express.type';
+import { Response, NextFunction } from 'express';
+
+export const getAllEvents = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { agendaId } = req.params as { agendaId: string };
+
+  try {
+    const events = await eventService.getAllEvents(agendaId);
+    res.status(200).json(events);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getEventById = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { eventId } = req.params as { eventId: string };
+  try {
+    const event = await eventService.getEventById(eventId);
+    res.status(200).json(event);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createEvent = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { agendaId } = req.params as { agendaId: string };
+  const eventData = req.body;
+
+  try {
+    const event = await eventService.createEvent(agendaId, eventData);
+    res.status(201).json(event);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateEvent = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { eventId } = req.params as { eventId: string };
+  const eventData = req.body;
+
+  try {
+    const event = await eventService.updateEvent(eventId, eventData);
+    res.status(200).json(event);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteEvent = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { eventId } = req.params as { eventId: string };
+
+  try {
+    await eventService.deleteEvent(eventId);
+    res.status(200).json({ message: 'Event deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
