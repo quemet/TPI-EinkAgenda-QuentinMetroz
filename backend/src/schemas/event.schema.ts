01d72141cfb57341472c619fb137e53565ee0475
Quentin Métroz <pg03lul@eduvaud.ch>
2026-05-13 08:44:19 +0200

    feat: add the route for the event

---
+++ backend/src/schemas/event.schema.ts
@@ -0,0 +1,35 @@
+import z from 'zod';
+
+const agendaBaseIdSchema = z.object({
+  agendaId: z.string().uuid(),
+});
+
+const eventBaseIdSchema = z.object({
+  eventId: z.string().uuid(),
+});
+
+const eventBaseSchemaBody = z.object({
+  name: z.string().min(1, 'Name is required'),
+  description: z.string().optional(),
+  type: z.string().min(1, 'Type is required'),
+  startDatetime: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: 'Invalid start datetime',
+  }),
+  endDatetime: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: 'Invalid end datetime',
+  }),
+});
+
+export const getAllEventSchemaParams = agendaBaseIdSchema;
+
+export const getEventByIdSchemaParams = eventBaseIdSchema;
+
+export const createEventSchemaParams = agendaBaseIdSchema;
+
+export const createEventSchemaBody = eventBaseSchemaBody;
+
+export const updateEventSchemaParams = eventBaseIdSchema;
+
+export const updateEventSchemaBody = eventBaseSchemaBody.partial();
+
+export const deleteEventSchemaParams = eventBaseIdSchema;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,42 @@
 import { initDatabase } from './config/db';
+import * as eventRouter from './routes/event.route';
+import express from 'express';
+import { env } from './config/env';
+import cors from 'cors';
+import { globalLimiter } from './middlewares/rateLimit.middleware';
+import { errorMiddleware } from './middlewares/error.middleware';
+import { notFoundMiddleware } from './middlewares/notFound.middleware';
 
-initDatabase()
-  .then(() => {
-    console.info('Database initialized successfully.');
-  })
-  .catch((error) => {
-    console.error('Failed to initialize database:', error);
-    process.exit(1);
-  });
+const app = express();
+const PORT = env.SERVER.PORT || 3000;
+
+app.use(
+  cors({
+    origin: env.SERVER.CORS,
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(globalLimiter);
+
+app.use('/api/events', eventRouter.default);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+if (env.NODE_ENV !== 'test') {
+  initDatabase()
+    .then(() => {
+      console.info('Database initialized successfully.');
+      app.listen(PORT, () => {
+        console.info(`Server is running on port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.error('Failed to initialize database:', error);
+      process.exit(1);
+    });
+}
+
+export default app;
